a0e0eba3c967074c1025074efde7e1145872116a
otreblan <otreblain@gmail.com>
2021-07-31 22:32:19 -0500

    Dump json

--- src/mini_prolog/trie_map.cpp
+++ src/mini_prolog/trie_map.cpp
@@ -14,11 +14,46 @@
 // You should have received a copy of the GNU General Public License
 // along with mini-prolog.  If not, see <http://www.gnu.org/licenses/>.
 
+#include <climits>
+
+#include <rapidjson/filewritestream.h>
+#include <rapidjson/writer.h>
+
 #include "trie_map.hpp"
 
 void ada::trie_map::dump(FILE* file) const
 {
-	// TODO
+	char buffer[PIPE_BUF];
+	rapidjson::FileWriteStream fws(file, buffer, sizeof(buffer));
+	rapidjson::Writer writer(fws);
+
+	writer.StartObject();
+
+	writer.Key("rules");
+	writer.StartArray();
+	for(const auto& [key, value]: tm)
+	{
+		writer.StartObject();
+
+		writer.Key("name");
+		writer.String(key.first);
+
+		writer.Key("length");
+		writer.Uint64(key.second);
+
+		writer.Key("sigma");
+		writer.String("TODO");
+
+		writer.Key("matrix");
+		writer.StartArray();
+		// TODO
+		writer.EndArray();
+
+		writer.EndObject();
+	}
+	writer.EndArray();
+
+	writer.EndObject();
 }
 
 void ada::trie_map::load(FILE* file)
